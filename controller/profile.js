9bf6ceb0c39bb5c1f603a5772dfd83c2cd087b6e
rahul kumar chaurasia <rahul.8d@gmail.com>
2019-04-29 21:22:24 +0530

    "bada boommm!!!!!!!!!!"

--- controller/profile.js
+++ controller/profile.js
@@ -1,93 +1,100 @@
-const Client = require('instagram-private-api').V1;
-const underscore = require('lodash');
-
-const config = require('../config.js').config;
-
-var device = new Client.Device('rahul');
-var storage = new Client.CookieFileStorage(config.cookiePath);
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 
 
-let accountId;
-var userMetaData = [];
-var numPosts =0
-
-// And go for login
-module.exports.getProfileData = (userHandle) => {
-    return Client.Session.create(device, storage, config.username, config.password)
-        .then(session => {
-            return Promise.all([session, Client.Account.searchForUser(session, userHandle)])
-        })
-        .spread((session, account) => {
-
-            userMetaData = account._params
-
-            accountId = account._params.id;
-
-            var feed = new Client.Feed.UserMedia(session, accountId);
-
-            return getPosts(feed);
-
-        })
-        .then(posts => {
-
-            return Promise.resolve({
-                "userMetaData": userMetaData,
-                "posts": posts
-            });
-        })
+var requiredPersonalData = ['biography',
+                            'external_url',
+                            'edge_followed_by.count',
+                            'edge_follow.count',
+                            'full_name',
+                            'id',
+                            'is_business_account',
+                            'is_joined_recently',
+                            'business_category_name',
+                            'is_private',
+                            'is_verified',
+                            'profile_pic_url_hd',
+                            'username'
+                            ]
 
 
+const getInfo = (user) => {
+    let out = {};
+
+    requiredPersonalData.forEach(field => {
+        let key = field.split('.');
+        let value;
+
+        if (key.length == 1) {
+            value = user[key[0]]
+        } else {
+
+            let temp = user
+            key.forEach(k => {
+                temp = temp[k]
+            })
+
+            value = temp
+        }
+
+        key = key[0]
+
+        out[key] = value
+
+    })
+
+    return out
 }
 
-let getPosts = (feed) => {
+const getFeedData= (feed) =>{
+    let out= []
 
-    var posts = []
+    total_posts= feed.count;
 
-    return feed.get()
-        .then(setPost)
-        .then(_posts => {
-
-            posts.push(_posts);
-
-        })
-        .then(_ => {
-
-            while (feed.isMoreAvailable() && numPosts < config.maxNumPost) {
-
-                feed.get()
-                    .then(_posts => {
-
-                        posts.push(_posts);
-
-                    })
-            }
-
-            return Promise.all(underscore.flatten(posts));
-        });
-
-}
-
-let setPost = (result) => {
-
-    return result.map(each => {
-        
-        numPosts ++ ;
-
-        x = each._params;
+    posts= feed.edges.map(post=>{
+        post= post.node;
 
         return {
-            'id': x.id,
-            'takenAt': x.takenAt,
-            'commentCount': x.commentCount,
-            'caption': x.caption,
-            'likeCount': x.likeCount,
-            'webLink': x.webLink,
+            id: post.id,
+            caption: post.edge_media_to_caption.edges[0].node.text,
+            shortcode: post.shortcode,
+            comments: post.edge_media_to_comment.count,
+            taken_at_timestamp: feed.taken_at_timestamp,
+            likes: post.edge_liked_by.count,
+            location: post.location,
+            img: post.thumbnail_src,
+            mentions: getMentions(post.edge_media_to_caption.edges[0].node.text)
+        }
+    }) ;
 
-            'images': x.images.map((each) => {
-                return each.url;
-            }),
-        };
+    return {
+        total_posts :total_posts,
+        posts: posts
+    }
+}
+
+const getMentions= caption =>{
+    pattern = /\B@[a-z0-9_-]+/gi ;   
+    
+    return caption.match(pattern);
+}
+
+module.exports.getProfileData = async (userHandle) => {
+
+    const response = await axios({
+        method: 'get',
+        url: `https://www.instagram.com/${userHandle}/`,
+        headers: {
+            Referer: `https://www.instagram.com/${userHandle}/`,
+        },
     });
 
+    const data = JSON.parse(response.data.match(/<script type="text\/javascript">window._sharedData = (.*);<\/script>/)[1]) || {};
+
+    let personalInfo = getInfo(data.entry_data.ProfilePage[0].graphql.user)
+
+    let postData = getFeedData(data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media)
+
+    return postData;
 }
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const profile = require('./controller/profile') ;
 profile.getProfileData('_ishwari_').then(o=>{
     console.log(JSON.stringify(o))
 });
+
+
+
